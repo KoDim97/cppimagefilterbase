f2b9e7ca233d45bca1b799e71aaa1ffe3aaa0835
KoDim97 <kodim97@yandex.ru>
2019-11-19 20:23:00 +0300

    Filters collections ver 1.6

--- filters.cpp
+++ filters.cpp
@@ -173,6 +173,7 @@ int Edge::getKernalValue(image_data& imgCopy, coefficients& matrix_area, coeffic
 				if (count == (numOfElements / 2)) {
 					sum += 9 * imgCopy.pixels[curPix];
 				}
+
 				else {
 					sum += -1 * imgCopy.pixels[curPix];
 				}
@@ -192,7 +193,7 @@ void Edge::setMonochrome(image_data& img, coefficients& area) {
 			curRed = img.pixels[curPix];
 			curGreen = img.pixels[curPix + 1];
 			curBlue = img.pixels[curPix + 2];
-			monochrome = 0.3 * curRed + 0.6 * curGreen + 0.1 * curBlue;
+			monochrome = (3 * curRed + 6 * curGreen + curBlue) / 10;
 			img.pixels[curPix] = monochrome;
 			img.pixels[curPix + 1] = monochrome;
 			img.pixels[curPix + 2] = monochrome;
